d9bc7a83a36556d73895f81db58ccb1fddca6170
lunash0 <huptential@naver.com>
2024-07-14 19:17:56 +0900

    last working day: 2023.08.26, upload

---
+++ cookie_ai_app/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+# 본질적으로, 모델이란 부가적인 메타데이터를 가진 데이터베이스의 구조(layout)를 말합니다.
+# Create your models here.
+from django.db import models
+class affected_area_img(models.Model):
